f67935f37fb8f228862101c4775c3c41a186e109
Gogs <gogs@fake.local>
2019-04-01 18:07:15 +0200

    package.json postuninstall

--- src/android/TekinWifi.java
+++ src/android/TekinWifi.java
@@ -15,18 +15,13 @@ public class TekinWifi extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("toggleWifi")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.toggleWifi(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void toggleWifi(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void toggleWifi(CallbackContext callbackContext) {
+        callbackContext.success("salut plugin");
     }
 }
