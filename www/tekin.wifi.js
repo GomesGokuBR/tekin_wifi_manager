2079304ffb8448432020f4cfe86ff34c10872bfc
gomesBR <e.gomesmatos@tekin.fr>
2019-10-25 15:18:35 +0200

    fix bug call func in tekin.wifi.js

--- www/tekin.wifi.js
+++ www/tekin.wifi.js
@@ -12,14 +12,14 @@ exports.connect = function (arg0, success, error) {
     exec(success, error, 'Wifi', 'connect', [arg0]);
 };
 
-exports.connect = function (arg0, success, error) {
-    exec(success, error, 'Wifi', 'gpsAndWifiState', [arg0]);
+exports.gpsAndWifiState = function (arg0, success, error) {
+    exec(success, error, 'Wifi', 'gpsAndWifiState', []);
 };
 
 exports.hasPermissions = function (success, error) {
     exec(success, error, 'Wifi', 'hasPermissions', []);
 };
 
-exports.hasPermissions = function (success, error) {
+exports.requestPermission = function (success, error) {
     exec(success, error, 'Wifi', 'requestPermission', []);
 };
